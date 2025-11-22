76a09c470d0a0919af93b63a2df1018f2ee8c6c1
Yman14 <ymanneil07@gmail.com>
2025-11-22 17:06:57 +0800

    create simple calculator to test calculator functions

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 const validate = document.querySelector("h1");
 validate.classList.add("validated");
+prototype();
 
 function add(a, b){
     return a+b;
 }
 
+function prototype(){
+    const a = parseInt(prompt("input first number: "));
+    const b = parseInt(prompt("input second number: "));
+    console.log(`result: ${add(a, b)}`);
+}
