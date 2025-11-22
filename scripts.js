f5d29d971b1a4ffbca44cbe6e69b571967a334a2
Yman14 <ymanneil07@gmail.com>
2025-11-22 17:09:32 +0800

    create function for substracting

--- scripts.js
+++ scripts.js
@@ -6,8 +6,14 @@ function add(a, b){
     return a+b;
 }
 
+function substract(a, b){
+    return a-b;
+}
+
 function prototype(){
     const a = parseInt(prompt("input first number: "));
     const b = parseInt(prompt("input second number: "));
+    console.log(`a: ${a} | b: ${b}`);
     console.log(`result: ${add(a, b)}`);
+    console.log(`result: ${substract(a, b)}`);
 }
