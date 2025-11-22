20068e55c7d6f81bb6929d334ea5aa861b76e9d4
Yman14 <ymanneil07@gmail.com>
2025-11-22 17:25:37 +0800

    create a separate function to take inputs from user

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 const validate = document.querySelector("h1");
 validate.classList.add("validated");
+
+//global
+let a, operator, b;
+
+//
 prototype();
+
 
 function add(a, b){
     return a+b;
@@ -21,18 +27,22 @@ function divide(a, b){
 
 //simple calculator for testing functions
 function prototype(){
-    const a = parseInt(prompt("input first number: "));
-    const b = parseInt(prompt("input second number: "));
-    console.log(`a: ${a} | b: ${b}`);
+    getInput();
+    console.log(`a: ${a} | operator: ${operator} | b: ${b}`);
     console.log(`add: ${add(a, b)}`);
     console.log(`substract: ${substract(a, b)}`);
     console.log(`multiply: ${multiply(a, b)}`);
     console.log(`divide: ${divide(a, b)}`);
 
     //get operator for manual calculation
-    operator = prompt("Operator: ");
     console.log(`manual: ${operate(a, operator, b)}`);
     
+}
+
+function getInput(){
+    a = parseInt(prompt("input first number: "));
+    operator = prompt("Operator: ");
+    b = parseInt(prompt("input second number: "));
 }
 
 function operate(a, operator, b){
